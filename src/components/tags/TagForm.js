fe121a578eb579f0655192016c3a3788523a730f
Emily Mai <emily.vy.mai@gmail.com>
2024-03-08 15:00:59 -0600

    tag form

---
+++ src/components/tags/TagForm.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { addTag } from "../../managers/tagManager"
+
+export const NewTagForm = () => {
+  const [tagLabel, setTagLabel] = useState([])
+
+  const navigate = useNavigate()
+
+  const handleFormChange = (event) => {
+    setTagLabel(event.target.value)
+  }
+
+  const handleAddTag = async () => {
+    if (tagLabel) {
+      const newTag = {
+        label: tagLabel,
+      }
+      addTag(newTag)
+      // navigate("/TagList")
+    } else {
+      alert("Please fill in all fields before adding a tag.")
+    }
+  }
+
+  return (
+    <div className="tag-form container">
+      <h2>New Tag</h2>
+      <form>
+        <label>Tag Label: </label>
+        <input
+          type="text"
+          placeholder="ex: JavaScript"
+          value={tagLabel}
+          onChange={handleFormChange}
+        />
+        <button onClick={handleAddTag}>Add Tag</button>
+      </form>
+    </div>
+  )
+}
