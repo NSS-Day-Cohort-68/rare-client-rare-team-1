6687a81a577809f455b41a67f82a3c0c3e3d60e0
Luke Williams <whatoftoday2002@gmail.com>
2024-03-12 14:33:09 -0500

    Created Logout component and fixed Login empty error bug

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,15 +1,13 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Logout = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
-    localStorage.removeItem('rare_user');
-    
-    
-    navigate('/login');
-  }, [navigate]);
+    localStorage.removeItem('rare_user')
+    navigate('/login')
+  }, [navigate])
 
-  return null;
+  return null
 };
