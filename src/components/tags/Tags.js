8f7aa693a278a7f9f8228a1b63d6a99adbee0e89
Claire Hanson <clairecatohanson@gmail.com>
2024-03-12 11:26:54 -0500

    display tags and move new tag form to /tags

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,16 +1,30 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { getSortedTags } from "../../managers/tagManager"
+import { NewTagForm } from "./TagForm"
+import "./Tags.css"
 
 export const Tags = () => {
-  const navigate = useNavigate()
+  const [tags, setTags] = useState([])
 
-  const handleClick = () => {
-    navigate("/new-tag")
-  }
+  useEffect(() => {
+    getSortedTags().then((res) => setTags(res))
+  }, [])
 
   return (
-    <div className="tag-list">
-      Tags
-      <button onClick={handleClick}>New Tag</button>
+    <div className="tags-global-container">
+      <div className="tag-list-container">
+        <h2>Tags</h2>
+        <ul className="tag-list">
+          {tags.map((t) => (
+            <div key={`tag-${t.id}`} className="tag-item">
+              <li className="tag-label">{t.label}</li>
+            </div>
+          ))}
+        </ul>
+      </div>
+      <div className="tag-form-container">
+        <NewTagForm />
+      </div>
     </div>
   )
 }
