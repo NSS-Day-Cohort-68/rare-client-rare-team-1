a867aa267e1f5f6c6782a2a22a4fdd749bb98390
Luke Williams <whatoftoday2002@gmail.com>
2024-03-15 11:41:51 -0500

    Added PostDetails and links

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -13,6 +13,6 @@ export const register_user = async (user) => {
 
 export const login_user = async (email) => {
   const response = await fetch(`${apiURL}/users?email=${email}`)
-  const data = await response.json()
-  return data
+  const token = await response.json()
+  return token
 }
